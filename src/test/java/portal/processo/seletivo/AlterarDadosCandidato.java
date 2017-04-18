b29ec4420e02ad137edf37a7a378ec1c0687a53e
TECHNE\renan.moura <renan.moura@techne.com.br>
2017-04-18 11:00:00 -0300

    Criação de um novo caso de teste  "Cadastro de composição do codigo da turma"

--- src/test/java/portal/processo/seletivo/AlterarDadosCandidato.java
+++ src/test/java/portal/processo/seletivo/AlterarDadosCandidato.java
@@ -12,9 +12,12 @@ public class AlterarDadosCandidato extends InicializarFirefox {
 	@Test
 	public void alterarDados (){
 		
-		getDriver().get("http://lyc-desenv:7030/processoseletivo/r/historico");
-		
-		getDriver().findElement(By.id("j_username")).sendKeys(userPortal);
+		getDriver().get(getPSPortal());
+
+		UtilidadesMetodos.implicitWait(30);
+		getDriver().findElement(By.xpath("//*[contains(text(), ' Login ')]")).click();
+		UtilidadesMetodos.implicitWait(30);
+		getDriver().findElement(By.id("j_username")).sendKeys("teste2326@6001.com");
 		getDriver().findElement(By.id("j_password")).sendKeys("123456789");
 		getDriver().findElement(By.id("j_enter")).click();
 		
@@ -27,19 +30,29 @@ public class AlterarDadosCandidato extends InicializarFirefox {
 		
 		UtilidadesMetodos.implicitWait(60);
 		getDriver().findElement(By.id("tipoPessoa")).click();
-		UtilidadesMetodos.tempo(5);
-		UtilidadesMetodos.implicitWait(60);
 		
-		getDriver().findElement(By.id("dddFone")).clear();
+		UtilidadesMetodos.tempo(6);
+		
+	
 		getDriver().findElement(By.id("dddFone")).sendKeys("22");
 		getDriver().findElement(By.id("dddCelular")).sendKeys("22");
 		getDriver().findElement(By.id("telefone")).sendKeys("222");
 		getDriver().findElement(By.id("celular")).sendKeys("2222222");
+		getDriver().findElement(By.id("email")).clear();
 		getDriver().findElement(By.id("email")).sendKeys(userPortal+".edit");
-					
-		UtilidadesMetodos.tempo(3);
+		UtilidadesMetodos.tempo(3);	
 	
+		getDriver().findElement(By.id("frm-pessoa-fisica")).submit();
 		
+		
+		UtilidadesMetodos.implicitWait(60);
+		//UtilidadesMetodos.tempo(3);
+		getDriver().findElement(By.xpath("//*[contains(., 'Ok')]")).click();
+		UtilidadesMetodos.implicitWait(60);
+		
+		getDriver().findElement(By.id("tipoSenha")).click();
+	
+		UtilidadesMetodos.tempo(3);
 		
 	}
 
---
+++ src/test/java/turmas/CadastroComposicaoCodTurma.java
@@ -0,0 +1,60 @@
+package turmas;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utilitarios.Produto.InicializarFirefox;
+import utilitarios.Produto.UtilidadesMetodos;
+
+public class CadastroComposicaoCodTurma  extends InicializarFirefox{
+	
+	@Test	
+	public void cadastroModCompTurma (){
+		
+		getDriver().get(getUrl()+"/Secretaria/Secretaria/turmas/TACDT055D.tp");
+		UtilidadesMetodos.users();
+		
+		UtilidadesMetodos.implicitWait(30);
+		UtilidadesMetodos.btnNovo();
+		getDriver().findElement(By.id("txbCodigo")).sendKeys("Grupo-Comp");
+		getDriver().findElement(By.id("txbDescricao")).sendKeys("Grupo de composição  de turma automatico");
+		UtilidadesMetodos.tempo(2);
+		UtilidadesMetodos.btnSalvar();
+		UtilidadesMetodos.tempo(7);
+		
+		
+	
+	
+		//registro contador
+	getDriver().findElement(By.xpath("//div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr/td/table/tbody/tr/td[5]/table/tbody/tr[2]/td[2]/em/button")).click();
+	getDriver().findElement(By.id("txtOrdemRECCT")).sendKeys("1");
+	WebElement drpCampo = getDriver().findElement(By.id("drpTipoRECCT"));
+	UtilidadesMetodos.buscaDrop(drpCampo, "Tabela");
+	WebElement drpColuna = getDriver().findElement(By.id("drpCampoRECCT"));
+	UtilidadesMetodos.buscaDrop(drpColuna, "CONTADOR");
+	getDriver().findElement(By.id("txtPosicaoInicialRECCT")).sendKeys("1");;
+	UtilidadesMetodos.tempo(3);
+	
+	getDriver().findElement(By.xpath("//div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[2]/em/button")).click();;
+	UtilidadesMetodos.tempo(3);
+	
+	//registro constante
+	
+	getDriver().findElement(By.xpath("//div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr/td/table/tbody/tr/td[5]/table/tbody/tr[2]/td[2]/em/button")).click();
+	getDriver().findElement(By.id("txtOrdemRECCT")).sendKeys("2");
+	WebElement drpCampo1 = getDriver().findElement(By.id("drpTipoRECCT"));
+	UtilidadesMetodos.buscaDrop(drpCampo1, "Tabela");
+	WebElement drpColuna2 = getDriver().findElement(By.id("drpCampoRECCT"));
+	UtilidadesMetodos.buscaDrop(drpColuna2, "CURSO");
+	getDriver().findElement(By.id("txtPosicaoInicialRECCT")).sendKeys("1");
+	getDriver().findElement(By.id("txtTamanhoRECCT")).sendKeys("5");
+	
+	UtilidadesMetodos.tempo(3);
+	
+	getDriver().findElement(By.xpath("//div[4]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[2]/em/button")).click();;
+	UtilidadesMetodos.tempo(3);
+
+	}
+
+}
